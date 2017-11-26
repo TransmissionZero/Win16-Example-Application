7e93a45392008f64a52a9bc3250603ac6b8f6a0c
Martin Payne <development@martinpayne.me.uk>
2017-11-26 20:06:57 +0000

    Converted resources to App Studio format so they can be edited via the
    GUI.

--- Resource.h
+++ Resource.h
@@ -1,15 +1,23 @@
-#ifndef RESOURCE_H
-#define RESOURCE_H
-
+//{{NO_DEPENDENCIES}}
+// App Studio generated include file.
+// Used by RESOURCE.RC
+//
 #define IDI_APPICON                     100
 #define IDR_MAINMENU                    101
 #define IDR_ACCELERATOR                 102
 #define IDD_ABOUTDIALOG                 103
-#define ID_FILE_EXIT                    100
-#define ID_HELP_ABOUT                   200
+#define ID_FILE_EXIT                    200
+#define ID_HELP_ABOUT                   300
+#define IDC_STATIC                      -1
 
-#ifndef IDC_STATIC
-  #define IDC_STATIC                    -1
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         301
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
-
 #endif
